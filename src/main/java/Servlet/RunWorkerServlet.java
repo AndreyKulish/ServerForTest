38020734ae82f5f95e603504f087731a3d69a0f3
HITEK <hitekdada@gmail.com>
2017-07-05 02:03:50 +0300

    Add update UI + thread Независимое многопоточное обновление Просто звиздец

--- src/main/java/Servlet/RunWorkerServlet.java
+++ src/main/java/Servlet/RunWorkerServlet.java
@@ -1,11 +1,17 @@
 package Servlet;
 
+import DBConfig.CRUDForURL;
+import DBConfig.MySQLCRUDForURLImpl;
+import DBConfig.TableEntity;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hitek on 03.07.2017.
@@ -15,23 +21,45 @@ public class RunWorkerServlet extends HttpServlet {
 
     private String FLAG;
     private boolean DO;
+    private Thread thread;
+    private TableEntity tableEntity;
+    private CRUDForURL crudForURL = new MySQLCRUDForURLImpl();
+    private static List<Thread> threadList = new ArrayList<>();
+    private static List<Work> tableWork = new ArrayList<>();
+    private Work work;
+
+    public static List<Thread> getThreadList() {
+        return threadList;
+    }
+
+    public static List<Work> getWork() {
+        return tableWork;
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         FLAG = request.getParameter("FLAG");
         if(FLAG.equals("START")){
+            threadList.clear();
             DO = true;
+            System.out.println(FLAG + "  " + DO);
+            for (TableEntity tableEntity :crudForURL.getAll()) {
+                work = new Work(tableEntity);
+                thread = new Thread(work);
+                tableWork.add(work);
+                thread.setName(Integer.toString(tableEntity.getID()));
+                threadList.add(thread);
+                thread.start();
+            }
         }else {
             DO = false;
-        }
-        System.out.println(FLAG + "" + DO);
-        while(DO) {
-            System.out.println("wvwvwevwevwevwe");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            System.out.println(FLAG + "  " + DO);
+            for (Thread thread : threadList) {
+                //мега костыль, its a magic
+                thread.setName("KILL_MY_PLZ");
             }
         }
+
+
     }
 
 }
